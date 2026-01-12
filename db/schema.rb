4032dfdedfc83b3fd684a9eba4429619d340be89
PavelAlexee <pavelps41980@gmail.com>
2026-01-12 22:49:38 +0300

    fixed for deploy17

--- db/schema.rb
+++ db/schema.rb
@@ -10,54 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_08_191518) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_12_194304) do
   create_table "categories", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 
   create_table "post_comments", force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.integer "user_id", null: false
+    t.string "ancestry", default: "", null: false
     t.text "content"
     t.datetime "created_at", null: false
+    t.integer "post_id", null: false
     t.datetime "updated_at", null: false
-    t.string "ancestry", default: "", null: false
+    t.integer "user_id", null: false
     t.index ["ancestry"], name: "index_post_comments_on_ancestry"
     t.index ["post_id"], name: "index_post_comments_on_post_id"
     t.index ["user_id"], name: "index_post_comments_on_user_id"
   end
 
   create_table "post_likes", force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.integer "user_id", null: false
     t.datetime "created_at", null: false
+    t.integer "post_id", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["post_id"], name: "index_post_likes_on_post_id"
     t.index ["user_id", "post_id"], name: "index_post_likes_on_user_id_and_post_id", unique: true
     t.index ["user_id"], name: "index_post_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
     t.text "body"
     t.integer "category_id", null: false
-    t.integer "creator_id", null: false
     t.datetime "created_at", null: false
+    t.integer "creator_id", null: false
+    t.string "title"
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_posts_on_category_id"
     t.index ["creator_id"], name: "index_posts_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name", null: false
+    t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
+    t.string "first_name", null: false
     t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
